181c7a31de1c541786dc61446645024ff029cbfc
shadow <panerusamrat@gmail.com>
2023-02-05 00:12:14 +0545

    Project Init

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {},
+    },
+  },
+  plugins: [],
+}
