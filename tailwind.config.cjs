5f185944f4392b24429806dfaa678ad2b6e115c0
shadowDarsan <51148380+shadowDarsan@users.noreply.github.com>
2023-04-12 10:20:26 +0545

    Tailwind configured

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,11 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'sm': {'min': '640px', 'max': '767px'},
-        'md': {'min': '768px', 'max': '1023px'},
-        'lg': {'min': '1024px', 'max': '1279px'},
-        'xl': {'min': '1280px', 'max': '1535px'},
-        '2xl': {'min': '1536px'},
+        sm: { min: '640px', max: '767px' },
+        md: { min: '768px', max: '1023px' },
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
       },
       colors: {},
     },
