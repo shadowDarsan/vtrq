94e900dc11490bb2246d43d9b728da6223003b98
shadow <panerusamrat@gmail.com>
2023-04-12 08:48:06 +0545

    upgraded tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,11 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
+        'sm': {'min': '640px', 'max': '767px'},
+        'md': {'min': '768px', 'max': '1023px'},
+        'lg': {'min': '1024px', 'max': '1279px'},
+        'xl': {'min': '1280px', 'max': '1535px'},
+        '2xl': {'min': '1536px'},
       },
       colors: {},
     },
